0127a79904ef79f0eae9ca9d94d74901746ceabf
tanakafwd <13965292+tanakafwd@users.noreply.github.com>
2021-11-19 23:25:14 +0900

    test case for pull request

--- tests/extension.test.js
+++ tests/extension.test.js
@@ -330,4 +330,56 @@ describe(
     timeoutMillis,
 );
 
-// TODO: Add tests for code blocks in a pull request.
+describe(
+    'Valid code blocks in the test pull request',
+    () => {
+      const testGitHubPageUrl = 'https://github.com/tanakafwd/mermaid-diagram-renderer/pull/1/files?short_path=85e72a6#diff-85e72a6a5d0579bda132364b7eea5cc3e4441f12dbe573be08cf2e8988705695';
+      const numCodeBlocksInTestGitHubPage = 7;
+      const numDiagramsInTestGitHubPage = 3;
+
+      let page;
+      beforeAll(async () => {
+        page = await global.__BROWSER__.newPage();
+        await page.goto(testGitHubPageUrl);
+      }, timeoutMillis);
+
+      afterAll( async () => {
+        await page.close();
+      });
+
+      it('should be the expected number of diagrams', async () => {
+        const numCodeBlocks = await page.evaluate(
+            () => document.querySelectorAll('pre[lang="mermaid"]').length);
+        expect(numCodeBlocks).toBe(numCodeBlocksInTestGitHubPage);
+      });
+
+      it('should be rendered as SVG', async () => {
+        for (let i = 1; i <= numDiagramsInTestGitHubPage; ++i) {
+          const diagramId = `mermaid-diagram-renderer-${i}`;
+          const tagName = await page.evaluate(
+              (targetId) => document.getElementById(targetId).tagName,
+              diagramId);
+          expect(tagName).toBe('svg');
+        }
+      });
+
+      it('should be rendered without error icon', async () => {
+        for (let i = 1; i <= numDiagramsInTestGitHubPage; ++i) {
+          const diagramId = `mermaid-diagram-renderer-${i}`;
+          const hasErrorIcon = await page.evaluate(
+              (targetId) => document.getElementById(targetId).querySelector(
+                  '[class="error-icon"]') !== null, diagramId);
+          expect(hasErrorIcon).toBeFalsy();
+        }
+      });
+
+      it('should be marked as processed', async () => {
+        const numCodeBlocks = await page.evaluate(
+            () => document.querySelectorAll(
+                'pre[lang="mermaid"][mermaid-diagram-renderer="processed"]',
+            ).length);
+        expect(numCodeBlocks).toBe(numDiagramsInTestGitHubPage);
+      });
+    },
+    timeoutMillis,
+);
